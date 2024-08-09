b2c9796d1cd8014cf750699d3afeb5fc3afad97f
PiPo <pipo.khomgrid@gmail.com>
2024-08-09 21:05:06 +0700

    test

--- app.mjs
+++ app.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 4000;
+const port = 4001;
 
 const data = {
   data: {
@@ -9,6 +9,9 @@ const data = {
     age: 20,
   },
 };
+app.get("/", (req, res) => {
+  return res.json("HELLO WORLD");
+});
 
 app.get("/profiles", (req, res) => {
   return res.json(data);
